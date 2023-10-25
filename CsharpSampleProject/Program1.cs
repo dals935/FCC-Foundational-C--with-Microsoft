da5ac66df7036f9ca08f3aa357f595f19794dc4f
John Ryan Andal <124016396+dals935@users.noreply.github.com>
2023-10-25 16:36:29 +0800

    Add files via upload

---
+++ CsharpSampleProject/Program1.cs
@@ -0,0 +1,223 @@
+﻿// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     total += 2;
+// }
+
+// if ((roll1 == roll2) && (roll2 == roll3)) 
+// {
+//     Console.WriteLine("You rolled triples! +6 bonus to total!");
+//     total += 6;
+// }
+
+// if (total >= 15)
+// {
+//     Console.WriteLine("You Win!");
+// }
+
+// if (total < 15)
+// {
+//     Console.WriteLine("Sorry, You Lose.");
+// }
+
+// string message = "The quick brown fox jumps over the lazy dog.";
+// bool result = message.Contains("dog");
+// Console.WriteLine(result);
+
+// if (message.Contains("fox"))
+// {
+//     Console.WriteLine("What does the fox say?");
+// }
+
+// string[] fraudulentOrderIDs = new string[3];
+
+// fraudulentOrderIDs[0] = "A123";
+// fraudulentOrderIDs[1] = "B456";
+// fraudulentOrderIDs[2] = "C789";
+// // fraudulentOrderIDs[3] = "D000";
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// fraudulentOrderIDs[0] = "F000";
+
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+// string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// fraudulentOrderIDs[0] = "F000";
+
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+// string[] names = { "Rowena", "Robin", "Bao" };
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// int[] inventory = { 200, 450, 700, 175, 250 };
+// int sum = 0;
+// int bin = 0;
+// foreach (int items in inventory)
+// {
+//     sum += items;
+//     bin++;
+//     Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+// }
+
+// Console.WriteLine($"We have {sum} items in inventory.");
+
+// string[] id = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+// foreach (string item in id)
+// {
+//     if(item.StartsWith("B"))
+//     {
+//         Console.WriteLine(item);
+//     }
+// }
+
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+// // Loop through each blank orderID
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     // Get a random value that equates to ASCII letters A through E
+//     int prefixValue = random.Next(65, 70);
+//     // Convert the random value into a char, then a string
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     // Create a random number, pad with zeroes
+//     string suffix = random.Next(1, 1000).ToString("000");
+//     // Combine the prefix and suffix together, then assign to current OrderID
+//     orderIDs[i] = prefix + suffix;
+// }
+// // Print out each orderID
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+
+// // Example 1:
+// Random dice = new Random();
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+// int total = roll1 + roll2 + roll3;
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+//     if ((roll1 == roll2) && (roll2 == roll3)) {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6; 
+//     } else {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+// string str = "The quick brown fox jumps over the lazy dog.";
+// // convert the message into a char array
+// char[] charMessage = str.ToCharArray();
+// // Reverse the chars
+// Array.Reverse(charMessage);
+// int x = 0;
+// // count the o's
+// foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// // convert it back to a string
+// string new_message = new String(charMessage);
+// // print it out
+// Console.WriteLine(new_message);
+// Console.WriteLine($"'o' appears {x} times.");
+
+// Console.WriteLine("a" == "a");
+// Console.WriteLine("a" == "A");
+// Console.WriteLine(1 == 2);
+
+// string myValue = "a";
+// Console.WriteLine(myValue == "a");
+
+// string value1 = " a";
+// string value2 = "A ";
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+// Console.WriteLine("a" != "a");
+// Console.WriteLine("a" != "A");
+// Console.WriteLine(1 != 2);
+
+// string myValue = "a";
+// Console.WriteLine(myValue != "a");
+
+// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
+
+// // These two lines of code will create the same output
+
+// Console.WriteLine(pangram.Contains("fox") == false);
+// Console.WriteLine(!pangram.Contains("fox"));
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(!pangram.Contains("fox"));
+// Console.WriteLine(!pangram.Contains("cow"));
+
+// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}");
+
+// int saleAmount = 1001;
+// // int discount = saleAmount > 1000 ? 100 : 50;
+
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+// Random coin = new Random();
+// Console.WriteLine((coin.Next(0, 2) == 0) ? "heads" : "tails");
+
+// string permission = "Admin|Manager";
+// int level = 53;
+
+// if (permission.Contains("Admin"))
+// {
+//     if (level > 55)
+//     {
+//         Console.WriteLine("Welcome, Super Admin user.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Welcome, Admin user.");
+//     }
+// }
+// else if (permission.Contains("Manager"))
+// {
+//     if (level >= 20)
+//     {
+//         Console.WriteLine("Contact an Admin for access.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("You do not have sufficient privileges.");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("You do not have sufficient privileges.");
+// }
